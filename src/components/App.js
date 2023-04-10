1ac75e6768baa9bb8f97bfc464eabb2a376116de
YassinMadara <YassinMadara@gmail.com>
2023-04-10 09:58:12 +0300

    Recipe Add/Delete buttons [using props]

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RecipeList from './RecipeList';
 import '../css/app.css'
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
-  return ( <RecipeList recipes = {sampleRecipes}/>)
+  const [recipes, setRecipes] = useState(sampleRecipes)
+
+  function handleRecipeAdd () {
+    const newRecipe = {
+      id: uuidv4(),
+      name: "Name",
+      servings: 1,
+      cookTime: "1:00",
+      instructions: "instru.",
+      ingredients: [
+        {
+          id: uuidv4(),
+          name:"ingre",
+          amount: "1 Tb"
+        }
+      ]
+    } 
+
+    setRecipes([...recipes, newRecipe])
+    
+  }
+
+  function handleRecipeDelete (id) {
+    setRecipes (recipes.filter(recipe => recipe.id !== id))
+  }
+  return ( <RecipeList 
+    recipes = {recipes}
+    handleRecipeAdd = {handleRecipeAdd}
+    handleRecipeDelete = {handleRecipeDelete}
+  />)
 }
 
 const sampleRecipes = [
