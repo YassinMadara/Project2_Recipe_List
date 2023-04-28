9aa4a2cb1b03eb0cd858cc86b7baf50846a86d9e
YassinMadara <YassinMadara@gmail.com>
2023-04-28 17:34:50 +0300

    adding RecipeEdit and styling it

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -15,7 +15,7 @@ export default function RecipeList({ recipes }) {
       </div>
       <div className='recipe-list__add-recipe-btn-container'>
         <button 
-        className='btn btn-primary'
+        className='btn btn--primary'
         onClick={handleRecipeAdd}
         >Add Recipe</button>
       </div>
